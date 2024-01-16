498a18dbf6ada78bae92d48c39cde28e8d3ce58b
Jason Kim <99324370+jason9288@users.noreply.github.com>
2024-01-16 15:39:18 +0900

    [Silver II] Title: 최대 힙, Time: 1036 ms, Memory: 30460 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(br.readLine());
+            if (temp == 0) {
+                if (maxHeap.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(maxHeap.poll());
+                }
+            } else {
+                maxHeap.add(temp);
+            }
+        }
+    }
+}
