c119e3e51c9767c0a394c76419c390086a073d5e
Jason Kim <99324370+jason9288@users.noreply.github.com>
2024-01-17 16:52:26 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 15572 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int time = 0;
+    static int[] dp;
+    public static void getMinTime(int pos) {
+        if (pos % 2 == 0) {
+            dp[pos] = Math.min(Math.min(dp[pos-1], dp[pos+1]), dp[pos/2]) + 1;
+        } else {
+            dp[pos] = Math.min(dp[pos-1], dp[pos+1]) + 1;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        dp = new int[100002];
+        for (int i = 0; i < 100002; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        for (int i = N; i >= 0; i--) {
+            dp[i] = N-i;
+        }
+        for (int i = N+1; i < 100001; i++) {
+            getMinTime(i);
+        }
+        for (int i = N+1; i < 100001; i++) {
+            getMinTime(i);
+        }
+        for (int i = N+1; i < 100001; i++) {
+            getMinTime(i);
+        }
+//
+//        for (int i = 0; i <= K+1; i++) {
+//            System.out.print(i + "까지 최소시간 : " + dp[i] + "\n");
+//        }
+//        System.out.println("\n" + dp[K]);
+
+        System.out.println(dp[K]);
+    }
+}
