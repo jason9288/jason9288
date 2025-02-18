2634e43845dd7483208dfc717edd1325d0cfc806
Kitae Kim <99324370+jason9288@users.noreply.github.com>
2025-02-18 21:35:15 +0900

    [Gold V] Title: 농장 관리, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Gold/1245. 농장 관리/농장 관리.cc
+++ 백준/Gold/1245. 농장 관리/농장 관리.cc
@@ -16,39 +16,28 @@ bool isValid(int r, int c) {
 }
 
 void checkH(int row, int col, int depth) {
-    // cout << endl;
-    // cout << depth << " : " << row << ", " << col << "방문 : 높이 " << farm[row][col] << endl;
 
     visited[row][col] = true;
     int h = farm[row][col];
 
-    // 주변 값들 탐색하며 만약 같은 레벨이면 재귀적으로 집합에 추가
     for (int i = 0; i < 8; i++) {
         int nxt_r = row + drow[i];
         int nxt_c = col + dcol[i];
         if (!isValid(nxt_r, nxt_c)) {
-            // cout << depth << " : " << nxt_r << ", " << nxt_c << "는 유효하지 않은 좌표\n";
             continue;
         }
-        // cout << depth << " : " <<  row << ", " << col << "의 " << "주변 좌표 탐색 " << nxt_r << ", " << nxt_c << ", 높이 : " << farm[nxt_r][nxt_c] << endl;
 
         if (farm[nxt_r][nxt_c] > h) {
-            // cout << depth << " : 주변에 나보다 높은 좌표 발견\n";
-            // 이 집합은 산봉우리가 될 수 없음
             bonguri = false;
         }
         if (farm[nxt_r][nxt_c] == h) {
             if (visited[nxt_r][nxt_c]) {
-                // cout << depth << " : 이미 집합에 포함됨\n";
                 continue;
             }
-            // cout << depth << " : 같은 높이 발견\n";
             checkH(nxt_r, nxt_c, depth+1);
         }
     }
-    // cout << depth << " : 현재 bool 값 " << bonguri << endl;
     if (depth == 0 && bonguri) {
-        // cout << depth << " : 주변 좌표 탐색 후 높은 좌표 없음; cnt 추가\n";
         cnt++;
     }
     return;
@@ -67,12 +56,6 @@ int main() {
             cin >> farm[i][j];
         }
     }
-    // for (int i = 0; i < n; i++) {
-    //     for (int j = 0; j < m; j++) {
-    //         cout << farm[i][j] << " ";
-    //     }
-    //     cout << endl;
-    // }
     
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
