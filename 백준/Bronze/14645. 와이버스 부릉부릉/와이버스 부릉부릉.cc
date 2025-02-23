73c0439ec0355323f4f182117d858139849a683c
Kitae Kim <99324370+jason9288@users.noreply.github.com>
2025-02-23 21:09:26 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    
+    while (n--) {
+        int a, b;
+        cin >> a >> b;
+    }
+    
+    cout << "비와이";
+}
